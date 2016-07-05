e8bd61d4f319b13e55fcce7b518222aa6421c96e
emmabeynon <emma.beynon@gmail.com>
2016-07-05 20:14:22 +0100

    Add ceremony start time

--- spec/features/ceremony_spec.rb
+++ spec/features/ceremony_spec.rb
@@ -10,4 +10,9 @@ feature 'Ceremony information' do
     visit '/'
     expect(page).to have_content('The Asylum Chapel, Caroline Gardens, Peckham, London, SE15 2SQ')
   end
+
+  scenario 'The ceremony start time is displayed' do
+    visit '/'
+    expect(page).to have_content('Arrive at 3.45pm for a 4pm start')
+  end
 end
