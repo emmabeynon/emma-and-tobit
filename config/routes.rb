8fd59f1585cbbfc473fcd8963c83b7f45eb3fef7
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2016-07-07 15:40:51 +0100

    Add welcome page for the index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'ceremony#index'
+  root 'welcome#index'
 
-  resources :ceremony, :reception, :accommodation, :rsvp
+  resources :ceremony, :reception, :accommodation, :rsvp, :welcome
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
